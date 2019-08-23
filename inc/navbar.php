1c45af81743da732b24a70955c8f714daaba1fa3
jomerlopez <54098499+jomerlopez@users.noreply.github.com>
2019-08-23 15:56:06 +0800

    Add files via upload

---
+++ inc/navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+  <a class="navbar-brand" href="index.php">User Account</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo ROOT_URL;?>">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo ROOT_URL;?>adduser.php">Add User</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+
+
+
